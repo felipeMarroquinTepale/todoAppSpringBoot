06203258ddaab1fcd3f164b244bb30ac817cc694
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2024-10-25 21:00:42 -0600

    first commit

---
+++ src/test/java/com/felipe/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
