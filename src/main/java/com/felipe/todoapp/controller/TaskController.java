f796000dc45ced619f5a10e8b0b58e7b1ee89a40
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2024-10-30 12:14:54 -0600

    subiendo funcionalidades restantes

--- src/main/java/com/felipe/todoapp/controller/TaskController.java
+++ src/main/java/com/felipe/todoapp/controller/TaskController.java
@@ -1,9 +1,14 @@
 package com.felipe.todoapp.controller;
 
 import com.felipe.todoapp.persistence.entity.Task;
+import com.felipe.todoapp.persistence.entity.TaskStatus;
 import com.felipe.todoapp.service.TaskService;
 import com.felipe.todoapp.service.dto.TaskInDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -19,5 +24,28 @@ public class TaskController {
     public Task createTask(@RequestBody TaskInDTO taskInDTO){
         return this.taskService.createTask(taskInDTO);
     }
+
+    @GetMapping
+    public List<Task> findAll(){
+        return this.taskService.findAll();
+    }
+
+    @GetMapping("/status/{status}")
+    public List<Task> findAllbyStatus(@PathVariable("status")TaskStatus status){
+        return this.taskService.findAllByTaskStatus(status);
+    }
+
+    @PatchMapping("/mark_as_finished/{id}")
+    public ResponseEntity<Void> markAsFiniched(@PathVariable("id") Long id){
+        this.taskService.updateTaskAsFinished(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+        this.taskService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/felipe/todoapp/service/TaskService.java
+++ src/main/java/com/felipe/todoapp/service/TaskService.java
@@ -1,10 +1,17 @@
 package com.felipe.todoapp.service;
 
+import com.felipe.todoapp.exceptions.ToDoExceptions;
 import com.felipe.todoapp.mapper.TaskInDTOToTask;
 import com.felipe.todoapp.persistence.entity.Task;
+import com.felipe.todoapp.persistence.entity.TaskStatus;
 import com.felipe.todoapp.persistence.repository.TaskRepository;
 import com.felipe.todoapp.service.dto.TaskInDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -19,5 +26,31 @@ public class TaskService {
     public Task createTask(TaskInDTO taskInDTO){
         Task task = mapper.map(taskInDTO);
         return this.repository.save(task);
+    }
+
+    //obtener todas las tareas
+    public List<Task> findAll(){
+        return this.repository.findAll();
+    }
+    //Obtener todas las tareas por estado
+    public List<Task> findAllByTaskStatus(TaskStatus status){
+        return this.repository.findAllByTaskStatus(status);
+    }
+
+    @Transactional
+    public void updateTaskAsFinished(Long id){
+        Optional<Task> optionalTask = this.repository.findById(id);
+        if(optionalTask.isEmpty()){
+            throw new ToDoExceptions("Task not found", HttpStatus.NOT_FOUND);
+        }
+        this.repository.markTaskAsFinished(id);
+    }
+
+    public void deleteById(Long id){
+        Optional<Task> optionalTask = this.repository.findById(id);
+        if(optionalTask.isEmpty()){
+            throw new ToDoExceptions("Task not found", HttpStatus.NOT_FOUND);
+        }
+        this.repository.deleteById(id);
     }
 }
